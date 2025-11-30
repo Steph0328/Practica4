ab81663c97264703bd99675c05d566ee58f86c2c
Steph0328 <smontero98@gmail.com>
2025-11-30 13:14:55 -0600

    Add project files.

---
+++ Controllers/EstudiantesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Practica4.BLL.Services;
+
+namespace Practica4.Web.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private readonly EstudianteService _service = new EstudianteService();
+
+        public ActionResult Index()
+        {
+            var lista = _service.ObtenerTodos();
+            return View(lista);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var estudiante = _service.ObtenerPorId(id);
+            if (estudiante == null) return HttpNotFound();
+            return View(estudiante);
+        }
+    }
+}
